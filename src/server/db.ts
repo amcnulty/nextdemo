00de1c2e410533ffad12afe36bc3f661cd987c3f
amcnulty <amcnulty88@swbell.net>
2023-10-04 15:16:21 -0500

    Generate

--- src/server/db.ts
+++ src/server/db.ts
@@ -6,11 +6,16 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+// console.log('globalForPrisma :>> ', globalForPrisma);
+// console.log('globalForPrisma.prisma :>> ', globalForPrisma.prisma);
+
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
+
+// console.log('db :>> ', db);
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
