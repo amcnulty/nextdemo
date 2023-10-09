1c2aaa2da912536ace94757e79c9c8fc12e825cf
Mcnulty <Aaron.McNulty1@va.gov>
2023-10-09 11:28:00 -0500

    Build Commit

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,10 +1,27 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import Layout from "~/components/layout";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Account() {
+  const { data: session } = useSession();
 
   return (
     <Layout>
-      <h1>Account page header</h1>
+      {session ? (
+        <>
+          <h1>Welcome {session.user.name}</h1>
+          <button className="btn btn-warning" onClick={() => signOut()}>
+            Sign In
+          </button>
+        </>
+      ) : (
+        <>
+          <h1>Account Login</h1>
+          <button className="btn btn-primary" onClick={() => signIn()}>
+            Sign In
+          </button>
+        </>
+      )}
     </Layout>
   );
 }
